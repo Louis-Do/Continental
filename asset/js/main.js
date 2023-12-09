c5248fbb979122ef72eba79155a858f48322da8a
mac <mac@eths-MacbookPro.local>
2023-12-10 03:36:47 +0700

    image continental

---
+++ asset/js/main.js
@@ -0,0 +1,14 @@
+// window.onload = function () {
+//     document.getElementById("name-btn").onclick = function () {
+//       document.getElementById("name-1").focus();
+//     };
+//   };
+function scrollToAbout() {
+  document.getElementById("about").scrollIntoView();
+}
+function scrollToMenu() {
+  document.getElementById("content").scrollIntoView();
+}
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView();
+}
